ddfc1ffa383c12f2bdfc22c86252fe814f5786de
ltkcl <167499996+ltkcl@users.noreply.github.com>
2026-03-01 16:45:40 +0530

    Remove serverUrl export from App.jsx
    Removed serverUrl export from App component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home.jsx"
-
-export const serverUrl= "http://localhost:5000"
-
 const App = () => {
   return (
     <div>
